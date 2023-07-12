3051a689213fbef6a1ef58d70c050adb92905a6b
Elias Kirkwood <ek.mindstorms@outlook.com>
2023-07-12 14:14:32 +0200

    Feat : working webview & packages

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
+/* import * as React from 'react'; */
+import React, { useState, useEffect, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet, BackHandler } from 'react-native';
+import { WebView } from 'react-native-webview';
+
 
 export default function App() {
+  const webViewRef = useRef(null);
+
+  useEffect(() => {
+    const backAction = () => {
+      webViewRef.current.goBack();
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction
+    );
+
+    return () => backHandler.remove();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='light' translucent={false} />
+      <WebView
+        source={{ uri: 'https://albums.grigwood.ml' }}
+        ref={webViewRef}
+        style={styles.view}
+        originWhitelist={['*']}
+        allowsInlineMediaPlayback
+        javaScriptEnabled
+        scalesPageToFit
+        mediaPlaybackRequiresUserAction={false}
+        javaScriptEnabledAndroid
+        useWebkit
+        startInLoadingState={true}
+        injectedJavaScript="// document.getElementsByClassName('h-12 lg:h-10')[0].src = 'https://ronde-de-l-espoir.fr/img/clean_logo_littoral.png'"
+      />
     </View>
   );
 }
@@ -14,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
